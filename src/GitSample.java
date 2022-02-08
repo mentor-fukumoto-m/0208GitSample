c43c87a6512b60efd5aff491b1c205de977726b2
M.fukumoto <m.fukumoto@one-smallstep.com>
2022-02-08 10:59:43 +0900

    main、メソッドを追加

--- src/GitSample.java
+++ src/GitSample.java
@@ -1,11 +1,11 @@
 
 public class GitSample {
+	public static void main(String[] args) {
 
-        System.out.println("中島です");
+		System.out.println("中島です");
 		System.out.println("片ノ坂です");
 		System.out.println("こんにちは！");
 		System.out.println("森田です");
 
-    }
-
+	}
 }
