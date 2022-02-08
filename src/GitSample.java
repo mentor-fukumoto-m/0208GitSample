07bd68dc9af50d9315e194ef67a9b3d9303ba2c4
A.nakashima <akira0219.tis2021@gmail.com>
2022-02-08 10:18:19 +0900

    更新

--- src/GitSample.java
+++ src/GitSample.java
@@ -1,9 +1,10 @@
 
 public class GitSample {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("福本です");
-	}
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("福本です");
+        System.out.println("中島です");
+    }
 
 }
