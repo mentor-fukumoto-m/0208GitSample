77a23bd2e3067e448bec7d67fdc9ee3f5e462868
M.morita <mmorita.tis2021@gmail.com>
2022-02-08 10:18:24 +0900

    あたらしい

--- src/GitSample.java
+++ src/GitSample.java
@@ -1,9 +1,10 @@
 
 public class GitSample {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("福本です");
-	}
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        System.out.println("福本です");
+        System.out.println("森田です");
+    }
 
 }
